68fe158f255c5796c9abbe2210077ce80b3167ae
MaxMorning <52603689+MaxMorning@users.noreply.github.com>
2022-03-09 08:54:11 +0800

    Update PipelineController.v

--- Code/Verilog/PipelineController.v
+++ Code/Verilog/PipelineController.v
@@ -18,8 +18,11 @@ module PipelineController (
 
     reg div_busy;
 
-    assign o_IF_ID_ena = ~i_ID_data_related_confict & ~i_MEM_answer_exc & ~div_busy & control_regs[3];
-    assign o_ID_EXE_ena = ~i_ID_data_related_confict & ~i_MEM_answer_exc & ~div_busy & control_regs[2];
+    reg[1:0] conflict_backup;
+    reg conflict_reg;
+
+    assign o_IF_ID_ena = ~i_MEM_answer_exc & ~div_busy & control_regs[3];
+    assign o_ID_EXE_ena = ~i_MEM_answer_exc & ~div_busy & control_regs[2];
     assign o_EXE_MEM_ena = ~i_MEM_answer_exc & ~div_busy & control_regs[1];
     assign o_MEM_WB_ena = ~div_busy & control_regs[0];
 
@@ -38,6 +41,25 @@ module PipelineController (
     always @(posedge clk) begin
         if (!resetn) begin
             control_regs <= 4'b1000;
+            conflict_reg <= 0;
+        end
+        else if (conflict_reg) begin
+            control_regs[3] <= conflict_backup[1];
+            control_regs[2] <= conflict_backup[0];
+            control_regs[1] <= control_regs[2];
+            control_regs[0] <= control_regs[1];
+
+            conflict_reg <= 0;
+        end
+        else if (i_ID_data_related_confict) begin
+            control_regs[3] <= 0;
+            control_regs[2] <= 0;
+            control_regs[1] <= control_regs[2];
+            control_regs[0] <= control_regs[1];
+
+            conflict_backup <= control_regs[3:2];
+
+            conflict_reg <= 1;
         end
         else if (~div_busy) begin
             control_regs[3] <= 1;
